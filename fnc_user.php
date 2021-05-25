0cc249b68f43270d73e24afbc74555a192c36f5b
Lisanne Järv <lisannejarv@gmail.com>
2021-05-25 21:03:15 +0300

    Update fnc_user.php

--- fnc_user.php
+++ fnc_user.php
@@ -1,27 +1,6 @@
 <?php
-$database = "if20_lisanne_ja_1" ;
+$database = "if20_lisanne" ;
 
-function signup($firstnameinput, $lastnameinput, $emailinput, $genderinput, $birthdate, $passwordinput){
-    $notice = null;
-    $conn = new mysqli ($GLOBALS ["serverhost"], $GLOBALS ["serverusername"], $GLOBALS ["serverpassword"], $GLOBALS ["database"] );
-    $stmt = $conn->prepare("INSERT INTO vpusers (firstname, lastname, birthdate, gender, email, password) VALUES (?,?,?,?,?,?)");
-    echo $conn->error;
-
-    //krupteerime salasona
-    $options = ["cost" => 12, "salt" => substr(sha1(rand()), 0, 22)];
-    $pwdhash = password_hash($passwordinput, PASSWORD_BCRYPT, $options);
-
-    $stmt->bind_param("sssiss", $firstnameinput, $lastnameinput, $birthdate, $genderinput, $emailinput, $pwdhash);
-
-    if($stmt->execute()){
-        $result = "ok";
-    } else {
-        $result = $stmt->error;
-    }
-    $stmt->close();
-    $conn->close();
-    return $result;
-}
 
 function signin($email, $password){
 	$notice = null;
